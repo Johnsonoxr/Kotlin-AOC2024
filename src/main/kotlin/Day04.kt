ef2d9231808b578f01bb3941a8715301df32beb1
Johnson1 Chang <Johnson1_Chang@asus.com>
2024-12-05 19:18:05 +0800

    day04

--- src/main/kotlin/Day04.kt
+++ src/main/kotlin/Day04.kt
@@ -3,22 +3,33 @@ import kotlin.system.measureTimeMillis
 fun main() {
 
     data class Node<T>(val v: T, val x: Int, val y: Int)
-    data class Graph<T>(val nodes: List<Node<T>>, val width: Int, val height: Int)
 
-    fun toGraph(input: List<String>): Graph<Char> {
-        val nodes = mutableListOf<Node<Char>>()
-        for ((y, line) in input.withIndex()) {
-            for ((x, c) in line.withIndex()) {
-                nodes.add(Node(c, x, y))
+    data class Graph<T>(val vMap: List<List<T>>, val width: Int, val height: Int) {
+        fun getNodeIterator(): Iterator<Node<T>> = object : Iterator<Node<T>> {
+            var x = 0
+            var y = 0
+            override fun hasNext() = y < height
+            override fun next(): Node<T> {
+                val node = Node(vMap[y][x], x, y)
+                x++
+                if (x == width) {
+                    x = 0
+                    y++
+                }
+                return node
             }
         }
-        return Graph(nodes, input[0].length, input.size)
+    }
+
+    fun toGraph(input: List<String>): Graph<Char> {
+        val charMap = input.map { it.toList() }
+        return Graph(charMap, input[0].length, input.size)
     }
 
     fun <T> Node<T>.move(graph: Graph<T>, dx: Int = 0, dy: Int = 0): Node<T>? {
         val x = this.x + dx
         val y = this.y + dy
-        return if (x in 0 until graph.width && y in 0 until graph.height) graph.nodes.first { it.x == x && it.y == y } else null
+        return graph.vMap.getOrNull(y)?.getOrNull(x)?.let { Node(it, x, y) }
     }
 
     fun part1(input: List<String>): Int {
@@ -36,13 +47,13 @@ fun main() {
             listOf(-1, -1)
         )
 
-        val mas = listOf('M', 'A', 'S')
+        val masList = listOf('M', 'A', 'S')
 
-        graph.nodes.forEach outer@{ node ->
+        graph.getNodeIterator().forEach outer@{ node ->
             if (node.v != 'X') return@outer
             directions.forEach inner@{ (dx, dy) ->
                 var currentNode: Node<Char>? = node
-                for (v in mas) {
+                for (v in masList) {
                     currentNode = currentNode?.move(graph, dx, dy) ?: return@inner
                     if (currentNode.v != v) return@inner
                 }
@@ -54,13 +65,32 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return 1
+        val graph = toGraph(input)
+        var count = 0
+
+        val msSet = setOf('M', 'S')
+
+        graph.getNodeIterator().forEach { node ->
+            if (node.v != 'A') return@forEach
+
+            val node11 = node.move(graph, -1, -1) ?: return@forEach
+            val node12 = node.move(graph, 1, 1) ?: return@forEach
+            if (setOf(node11.v, node12.v) != msSet) return@forEach
+
+            val node21 = node.move(graph, -1, 1) ?: return@forEach
+            val node22 = node.move(graph, 1, -1) ?: return@forEach
+            if (setOf(node21.v, node22.v) != msSet) return@forEach
+
+            count++
+        }
+
+        return count
     }
 
     val testInput = readInput("04t")
     val input = readInput("04")
 
-    check(part1(testInput).also { it.println() } == 18)
+    check(part1(testInput) == 18)
 
     val time1 = measureTimeMillis {
         part1(input).println()
@@ -68,7 +98,7 @@ fun main() {
 
     "Part 1 completed in $time1 ms".println()
 
-    check(part2(testInput) == 1)
+    check(part2(testInput) == 9)
 
     val time2 = measureTimeMillis {
         part2(input).println()
