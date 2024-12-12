6ef0c11d71af757ec7e297bac16f9cbae32e2076
Johnson1 Chang <Johnson1_Chang@asus.com>
2024-12-12 22:50:07 +0800

    day12

--- src/main/kotlin/Day12.kt
+++ src/main/kotlin/Day12.kt
@@ -36,51 +36,92 @@ fun main() {
         )
     }
 
-    fun part1(input: List<String>): Int {
-
-        val gardenGraph = Graph(input.map { line -> line.toList().map { it } })
-
-        fun Node<Char>.goConnectedSamePlants(): Set<Node<Char>> {
-            val visited: MutableSet<Node<Char>> = mutableSetOf(this)
-            var uncheckedSet: Set<Node<Char>> = setOf(this)
-            while (uncheckedSet.isNotEmpty()) {
-                val newUncheckedSet = mutableSetOf<Node<Char>>()
-                uncheckedSet.map { it.neighbors(gardenGraph) }.flatten().distinct().forEach { uncheckedNeighbor ->
-                    if (uncheckedNeighbor.v == v && visited.add(uncheckedNeighbor)) {
-                        newUncheckedSet += uncheckedNeighbor
-                    }
+    fun Node<Char>.getConnectedPlants(gardenGraph: Graph<Char>): Set<Node<Char>> {
+        val visited: MutableSet<Node<Char>> = mutableSetOf(this)
+        var uncheckedSet: Set<Node<Char>> = setOf(this)
+        while (uncheckedSet.isNotEmpty()) {
+            val newUncheckedSet = mutableSetOf<Node<Char>>()
+            uncheckedSet.map { it.neighbors(gardenGraph) }.flatten().distinct().forEach { uncheckedNeighbor ->
+                if (uncheckedNeighbor.v == v && visited.add(uncheckedNeighbor)) {
+                    newUncheckedSet += uncheckedNeighbor
                 }
-                uncheckedSet = newUncheckedSet
             }
-            return visited
+            uncheckedSet = newUncheckedSet
         }
+        return visited
+    }
+
+    fun part1(input: List<String>): Int {
+        val gardenGraph = Graph(input.map { line -> line.toList().map { it } })
 
         var cost = 0
 
         var checkedPlants = setOf<Node<Char>>()
         while (true) {
-            val plant = gardenGraph.getNodeIterator().asSequence().firstOrNull { it !in checkedPlants } ?: break
-            val connectedSamePlants = plant.goConnectedSamePlants()
-            checkedPlants = checkedPlants + connectedSamePlants
-            val area = connectedSamePlants.size
-            val boundaries = connectedSamePlants.sumOf {
-                val neighbors = it.neighbors(gardenGraph)
-                neighbors.count { n -> n.v != it.v } + 4 - neighbors.size
+            val unhandledPlant = gardenGraph.getNodeIterator().asSequence().firstOrNull { it !in checkedPlants } ?: break
+            val connectedPlants = unhandledPlant.getConnectedPlants(gardenGraph)
+            val area = connectedPlants.size
+            val boundaries = connectedPlants.sumOf { p ->
+                val neighborPlants = p.neighbors(gardenGraph)
+                neighborPlants.count { np -> np.v != p.v } + 4 - neighborPlants.size
             }
             cost += area * boundaries
+            checkedPlants = checkedPlants + connectedPlants
         }
 
         return cost
     }
 
     fun part2(input: List<String>): Int {
-        return 1
+        var inputSurroundedByStars = listOf("*".repeat(input[0].length)) + input + listOf("*".repeat(input[0].length))
+        inputSurroundedByStars = inputSurroundedByStars.map { "*$it*" }
+
+        val gardenGraph = Graph(inputSurroundedByStars.map { line -> line.toList().map { it } })
+
+        data class Boundary(val sidePlant1: Node<Char>, val sidePlant2: Node<Char>) {
+            fun getSortableHash(): Int {
+                return if (sidePlant1.x == sidePlant2.x) {
+                    if (sidePlant1.y > sidePlant2.y)
+                        sidePlant1.x + 1000 * sidePlant1.y + 0
+                    else
+                        sidePlant1.x + 1000 * sidePlant1.y + 1000000
+                } else {
+                    if (sidePlant1.x > sidePlant2.x)
+                        sidePlant1.y + 1000 * sidePlant1.x + 2000000
+                    else
+                        sidePlant1.y + 1000 * sidePlant1.x + 3000000
+                }
+            }
+        }
+
+        var cost = 0
+
+        var checkedPlants = setOf<Node<Char>>()
+        while (true) {
+            val unhandledPlant = gardenGraph.getNodeIterator().asSequence().firstOrNull { it.v != '*' && it !in checkedPlants } ?: break
+            val connectedPlants = unhandledPlant.getConnectedPlants(gardenGraph)
+            val area = connectedPlants.size
+            val boundaries = connectedPlants.map { p ->
+                p.neighbors(gardenGraph).filter { np -> np.v != p.v }.map { np -> Boundary(np, p) }
+            }.flatten().toMutableSet()
+
+            var discontinuousBoundaryCount = 1
+            boundaries.map { it.getSortableHash() }.sorted().windowed(2) { (hash1, hash2) ->
+                if (hash2 - hash1 > 1) {
+                    discontinuousBoundaryCount++
+                }
+            }
+            cost += area * discontinuousBoundaryCount
+
+            checkedPlants = checkedPlants + connectedPlants
+        }
+
+        return cost
     }
 
-    val testInput = readInput("12t")
     val input = readInput("12")
 
-    check(part1(testInput) == 1930)
+    check(part1(readInput("12t")) == 1930)
 
     val time1 = measureTimeMillis {
         part1(input).println()
@@ -88,7 +129,11 @@ fun main() {
 
     "Part 1 completed in $time1 ms".println()
 
-    check(part2(testInput) == 1)
+    check(part2(readInput("12t0")) == 80)
+    check(part2(readInput("12t1")) == 436)
+    check(part2(readInput("12t2")) == 236)
+    check(part2(readInput("12t3")) == 368)
+    check(part2(readInput("12t")) == 1206)
 
     val time2 = measureTimeMillis {
         part2(input).println()
