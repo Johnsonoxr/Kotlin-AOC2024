e162680b4887abfdec13b6308b339a5f65b6b7be
Johnson1 Chang <Johnson1_Chang@asus.com>
2024-12-17 09:44:37 +0800

    day16

--- src/main/kotlin/Day16.kt
+++ src/main/kotlin/Day16.kt
@@ -4,6 +4,7 @@ fun main() {
 
     data class Position(val x: Int, val y: Int) {
         operator fun plus(xy: Position): Position = Position(x + xy.x, y + xy.y)
+        operator fun minus(xy: Position): Position = Position(x - xy.x, y - xy.y)
     }
 
     data class Node<T>(val v: T, val p: Position)
@@ -37,102 +38,31 @@ fun main() {
         }
     }
 
-    val east = Position(-1, 0)
-    val north = Position(0, -1)
-    val west = Position(1, 0)
-    val south = Position(0, 1)
-
-    val dirCharList = listOf(
-        '>',
-        'v',
-        '<',
-        '^',
-    )
+    data class Move(val p: Position, val d: Position, val score: Int)
 
     val dirList = listOf(
-        west,
-        south,
-        east,
-        north
+        Position(1, 0),
+        Position(0, 1),
+        Position(-1, 0),
+        Position(0, -1)
     )
-
-    fun getPositionOffsetFromDirection(dir: Char) = dirList[dirCharList.indexOf(dir)]
-
-    fun _part1(input: List<String>): Int {
-        val graph = Graph(input.map { it.toMutableList() })
-        val end = graph.getNodeIterator().asSequence().find { it.v == 'E' }!!
-        val start = graph.getNodeIterator().asSequence().find { it.v == 'S' }!!
-        graph[end.p] = '.'
-
-        fun <T> Position.findValidDirections(graph: Graph<T>): List<Char> = listOfNotNull(
-            if (graph[this + west] == '.') '>' else null,
-            if (graph[this + south] == '.') 'v' else null,
-            if (graph[this + east] == '.') '<' else null,
-            if (graph[this + north] == '.') '^' else null,
-        )
-
-        data class PositionDir(val p: Position, val d: Char)
-
-        val bestPositionScore = mutableMapOf<PositionDir, Int>()
-
-        fun evalScore(p: Position, dir: Char, graph: Graph<Char>, accumulatedScore: Int): Int? {
-            if (p == end.p) {
-                "Path found with score $accumulatedScore".println()
-                return 0
-            }
-
-            val pDir = PositionDir(p, dir)
-            if (bestPositionScore[pDir]?.let { it <= accumulatedScore } == true) {
-                return null
-            }
-            bestPositionScore[pDir] = accumulatedScore
-
-            val nextGraph = Graph(graph.vMap.map { it.toMutableList() }).also { it[p] = dir }
-
-            val nextDirs = p.findValidDirections(nextGraph)
-            if (nextDirs.isEmpty()) return null
-
-            val scores = nextDirs.shuffled().mapNotNull { nextDir ->
-                val stepScore = when (nextDir) {
-                    dir -> 1
-                    else -> when {
-                        dir == '<' && nextDir == '>' -> 2001
-                        dir == '^' && nextDir == 'v' -> 2001
-                        dir == '>' && nextDir == '<' -> 2001
-                        dir == 'v' && nextDir == '^' -> 2001
-                        else -> 1001
-                    }
-                }
-                evalScore(p + getPositionOffsetFromDirection(nextDir), nextDir, nextGraph, accumulatedScore + stepScore)?.let {
-                    it + stepScore
-                }
-            }
-            return scores.minOrNull()
-        }
-
-        val minScore = evalScore(start.p, '<', graph, 0)!!
-
-        return minScore
-    }
 
     fun part1(input: List<String>): Int {
         val graph0 = Graph(input.map { it.toMutableList() })
         val end = graph0.getNodeIterator().asSequence().find { it.v == 'E' }!!
         val start = graph0.getNodeIterator().asSequence().find { it.v == 'S' }!!
 
-        data class Movement(val p: Position, val d: Char, val score: Int)
+        val graph: Graph<MutableMap<Position, Int>?> =  //  map of direction to score
+            Graph(graph0.vMap.map { it.map { dir -> if (dir == '#') null else mutableMapOf<Position, Int>() }.toMutableList() })
 
-        val graph: Graph<MutableMap<Char, Int>?> =  //  map of direction to score
-            Graph(graph0.vMap.map { it.map { dir -> if (dir == '#') null else mutableMapOf<Char, Int>() }.toMutableList() })
-
-        var moves = listOf(Movement(start.p, '<', 0))
+        var moves = setOf(Move(start.p, Position(1, 0), 0))
 
         while (moves.isNotEmpty()) {
-            val nextMoves = mutableListOf<Movement>()
+            val nextMoves = mutableSetOf<Move>()
             moves.forEach { move ->
-                val forward = move.copy(p = move.p + getPositionOffsetFromDirection(move.d), score = move.score + 1)
-                val clockwise = move.copy(d = dirCharList[(dirCharList.indexOf(move.d) + 1) % 4], score = move.score + 1000)
-                val counterClockwise = move.copy(d = dirCharList[(dirCharList.indexOf(move.d) + 3) % 4], score = move.score + 1000)
+                val forward = move.copy(p = move.p + move.d, score = move.score + 1)
+                val clockwise = move.copy(d = dirList[(dirList.indexOf(move.d) + 1) % 4], score = move.score + 1000)
+                val counterClockwise = move.copy(d = dirList[(dirList.indexOf(move.d) + 3) % 4], score = move.score + 1000)
                 listOf(
                     forward,
                     clockwise,
@@ -151,7 +81,63 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return 1
+        val graph0 = Graph(input.map { it.toMutableList() })
+        val end = graph0.getNodeIterator().asSequence().find { it.v == 'E' }!!
+        val start = graph0.getNodeIterator().asSequence().find { it.v == 'S' }!!
+
+        val graph: Graph<MutableMap<Position, Int>?> =  //  map of direction to score
+            Graph(graph0.vMap.map { it.map { dir -> if (dir == '#') null else mutableMapOf<Position, Int>() }.toMutableList() })
+
+        var moves = setOf(Move(start.p, Position(1, 0), 0))
+
+        while (moves.isNotEmpty()) {
+            val nextMoves = mutableSetOf<Move>()
+            moves.forEach { move ->
+                val forward = move.copy(p = move.p + move.d, score = move.score + 1)
+                val cw = move.copy(d = dirList[(dirList.indexOf(move.d) + 1) % 4], score = move.score + 1000)
+                val ccw = move.copy(d = dirList[(dirList.indexOf(move.d) + 3) % 4], score = move.score + 1000)
+                listOf(
+                    forward,
+                    cw,
+                    ccw
+                ).forEach nextMoveCheck@{ nextMove ->
+                    val dirMap = graph[nextMove.p] ?: return@nextMoveCheck
+                    if (nextMove.score >= (dirMap[nextMove.d] ?: Int.MAX_VALUE)) return@nextMoveCheck
+                    dirMap[nextMove.d] = nextMove.score
+                    nextMoves.add(nextMove)
+                }
+            }
+            moves = nextMoves
+        }
+
+        val lowestScore = graph[end.p]!!.values.min()
+        moves = graph[end.p]!!.filter { it.value == lowestScore }.map { Move(end.p, it.key, lowestScore) }.toSet()
+
+        val canvasGraph = Graph(input.map { it.toMutableList() })
+
+        while (moves.isNotEmpty()) {
+            val nextMoves = mutableSetOf<Move>()
+            moves.map { move ->
+                canvasGraph[move.p] = 'O'
+
+                val backward = move.copy(p = move.p - move.d, score = move.score - 1)
+                val ccw = move.copy(d = dirList[(dirList.indexOf(move.d) + 1) % 4], score = move.score - 1000)
+                val cw = move.copy(d = dirList[(dirList.indexOf(move.d) + 3) % 4], score = move.score - 1000)
+                listOf(
+                    backward,
+                    ccw,
+                    cw
+                ).forEach nextMoveCheck@{ nextMove ->
+                    val dirMap = graph[nextMove.p] ?: return@nextMoveCheck
+                    if (nextMove.score != dirMap[nextMove.d]) return@nextMoveCheck
+                    nextMoves.add(nextMove)
+                }
+            }
+            moves = nextMoves
+        }
+        canvasGraph[start.p] = 'O'
+
+        return canvasGraph.getNodeIterator().asSequence().count { it.v == 'O' }
     }
 
     val testInput = readInput("16t")
@@ -165,7 +151,7 @@ fun main() {
 
     "Part 1 completed in $time1 ms".println()
 
-    check(part2(testInput) == 1)
+    check(part2(testInput) == 64)
 
     val time2 = measureTimeMillis {
         part2(input).println()
